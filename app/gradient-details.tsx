ca20cfb5628b67da37f7e72e6434d0783f43eb10
Sumer Patel <sumerpatel.dev@gmail.com>
2024-10-25 23:41:23 -0700

    Dynamic chnages to Gradient Details

--- app/gradient-details.tsx
+++ app/gradient-details.tsx
@@ -1,10 +1,35 @@
-import { Redirect, useLocalSearchParams } from 'expo-router';
+import { LinearGradient, LinearGradientPoint } from 'expo-linear-gradient';
+import { Redirect, Stack, useLocalSearchParams } from 'expo-router';
 
-import { StyleSheet, ScrollView, Platform } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView, Platform, View, TouchableOpacity } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText'
+import { ThemedText } from '@/components/ThemedText';
+import { Ionicons } from '@expo/vector-icons'
+
+type GradientDirection =
+  | 'topToBottom'
+  | 'bottomToTop'
+  | 'leftToRight'
+  | 'rightToLeft';
+
+interface GradientDirections {
+  [key: string]: {
+    start: LinearGradientPoint;
+    end: LinearGradientPoint;
+  };
+}
+
+const gradientDirections: GradientDirections = {
+  topToBottom: { start: { x: 0, y: 0 }, end: { x: 0, y: 1 } },
+  bottomToTop: { start: { x: 0, y: 1 }, end: { x: 0, y: 0 } },
+  leftToRight: { start: { x: 0, y: 0 }, end: { x: 1, y: 0 } },
+  rightToLeft: { start: { x: 1, y: 0 }, end: { x: 0, y: 0 } },
+};
 
 export default function GradientDetailsScreen() {
+  const [direction, setDirection] = useState<GradientDirection>('topToBottom');
+
   const { colors, gradientName } = useLocalSearchParams<{ colors: string, gradientName: string }>();
 
   if (!gradientName || !colors) {
@@ -13,17 +38,70 @@ export default function GradientDetailsScreen() {
   }
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <ThemedText>{gradientName}</ThemedText>
-    </ScrollView>
+    <>
+      <Stack.Screen options={{ title: gradientName }} />
+      <ScrollView contentContainerStyle={styles.container}>
+        <LinearGradient
+          colors={colors.split(',')}
+          style={StyleSheet.absoluteFill}
+          start={gradientDirections[direction].start}
+          end={gradientDirections[direction].end}
+        />
+        <View
+          style={{
+            width: '100%',
+            alignSelf: 'center',
+            position: 'absolute',
+            bottom: 30,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              gap: 15,
+              marginBottom: 12,
+              alignSelf: 'center',
+            }}
+          >
+            <ThemedText type='defaultSemiBold'>{direction}</ThemedText>
+            <ThemedText type='defaultSemiBold'>{colors.replace(',', ' - ')}</ThemedText>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
+            }}
+          >
+            <TouchableOpacity onPress={() => setDirection('topToBottom')}>
+              <Ionicons name='chevron-down-circle' size={40} color={'white'} />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => setDirection('bottomToTop')}>
+              <Ionicons name='chevron-up-circle' size={40} color={'white'} />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => setDirection('leftToRight')}>
+              <Ionicons
+                name='chevron-forward-circle'
+                size={40}
+                color={'white'}
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => setDirection('rightToLeft')}>
+              <Ionicons name='chevron-back-circle' size={40} color={'white'} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: Platform.OS === 'ios' ? 140 : 0,
+    flex: 1,
+    paddingTop: Platform.OS === 'ios' ? 150 : 0,
+    padding: 16,
   },
-  gradientsContainer: {
-    flexDirection: 'row',
-  }
 });
